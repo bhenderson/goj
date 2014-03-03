40f001a295a8f67d614907c1faae3d7a91638753
bhenderson <bhenderson@yp.com>
2014-03-03 08:57:20 -0800

    Indention.

--- color.go
+++ color.go
@@ -13,45 +13,66 @@ var (
 	yellow = ansi.ColorFunc("yellow")
 )
 
+type indent struct {
+	prefix, indent string
+	depth          int
+}
+
 func (d Decoder) MarshalJSON() ([]byte, error) {
 	var buf bytes.Buffer
-	err := colorize(&buf, d.v)
+	err := colorize(&buf, d.v, &indent{indent: "  "})
 	return buf.Bytes(), err
 }
 
-func colorize(buf *bytes.Buffer, v interface{}) (err error) {
+func colorize(buf *bytes.Buffer, v interface{}, idt *indent) (err error) {
 	var b []byte
+	var p int
 
 	switch x := v.(type) {
 	case map[string]interface{}:
 		// json keys must be strings
-		buf.WriteRune('{')
+		idt.depth++
+		buf.WriteByte('{')
 		for k, val := range x {
+			newline(buf, idt)
 			b, err = json.Marshal(k)
 			if err != nil {
 				return err
 			}
 			buf.WriteString(blue(string(b)))
-			buf.WriteRune(':')
-			err = colorize(buf, val)
+			buf.WriteByte(':')
+			buf.WriteByte(' ')
+			err = colorize(buf, val, idt)
 			if err != nil {
 				return err
 			}
-			buf.WriteRune(',')
+			p = buf.Len()
+			buf.WriteByte(',')
 		}
-		buf.Truncate(buf.Len() - 1) // last ,
-		buf.WriteRune('}')
+		idt.depth--
+		if p != 0 {
+			buf.Truncate(buf.Len() - 1) // last ,
+			newline(buf, idt)
+		}
+		buf.WriteByte('}')
 	case []interface{}:
-		buf.WriteRune('[')
+		idt.depth++
+		buf.WriteByte('[')
 		for _, val := range x {
-			err = colorize(buf, val)
+			newline(buf, idt)
+			err = colorize(buf, val, idt)
 			if err != nil {
 				return err
 			}
-			buf.WriteRune(',')
+			p = buf.Len()
+			buf.WriteByte(',')
 		}
-		buf.Truncate(buf.Len() - 1) // last ,
-		buf.WriteRune(']')
+		idt.depth--
+		if p != 0 {
+			buf.Truncate(buf.Len() - 1) // last ,
+			newline(buf, idt)
+		}
+		buf.WriteByte(']')
 	case int, float64:
 		b, err = json.Marshal(x)
 		if err != nil {
@@ -78,4 +99,12 @@ func colorize(buf *bytes.Buffer, v interface{}) (err error) {
 		buf.Write(b)
 	}
 	return
+}
+
+func newline(buf *bytes.Buffer, idt *indent) {
+	buf.WriteByte('\n')
+	buf.WriteString(idt.prefix)
+	for i := 0; i < idt.depth; i++ {
+		buf.WriteString(idt.indent)
+	}
 }
