ce6a7469142879405cdeb2642e2f224f3715e2d0
bhenderson <bhenderson@yp.com>
2014-03-03 09:50:07 -0800

    Use escape sequences in test.

--- goj_test.go
+++ goj_test.go
@@ -12,8 +12,15 @@ func Test_Decode(t *testing.T) {
 		"n":null,
 		"a":["b",1]
 	}`
-	output :=
-		"{\n  \x1b[34m\"hi\"\x1b[0m: \x1b[32m\"mom\"\x1b[0m,\n  \x1b[34m\"n\"\x1b[0m: \x1b[1;30mnull\x1b[0m,\n  \x1b[34m\"a\"\x1b[0m: [\n    \x1b[32m\"b\"\x1b[0m,\n    \x1b[33m1\x1b[0m\n  ]\n}"
+
+	output := `{
+  [34m"hi"[0m: [32m"mom"[0m,
+  [34m"n"[0m: [1;30mnull[0m,
+  [34m"a"[0m: [
+    [32m"b"[0m,
+    [33m1[0m
+  ]
+}`
 
 	r := strings.NewReader(input)
 	dec := NewDecoder(r)
