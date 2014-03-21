448cc2ae474b555cdc7277a02e33ead7f4ee4650
bhenderson <bhenderson@yp.com>
2014-03-21 09:23:59 -0700

    Extract test helper method.

--- filter_test.go
+++ filter_test.go
@@ -26,9 +26,9 @@ var input = `{
 
 func TestPath_FilterOn(t *testing.T) {
 	var exp string
-	var err error
-	var d1, d2 *Decoder
+	var e, a interface{}
 
+	// parent
 	exp = `{
 		"store": {
 			"bicycles": [
@@ -41,26 +41,13 @@ func TestPath_FilterOn(t *testing.T) {
 			}
 		}
 	}`
+	e, a = testFilterOn(t, exp, input, "**.price=3.99..color")
+	assert.Equal(t, e, a)
 
-	// parent
-	d1 = testDecoder(t, exp)
-	d2 = testDecoder(t, input)
-	err = d2.FilterOn("**.price=3.99..color")
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assert.Equal(t, d1.v, d2.v)
-
-	d2 = testDecoder(t, input)
-	err = d2.FilterOn("blah")
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assert.Equal(t, nil, d2.v)
+	// wrong filter, returns nil
+	exp = `null`
+	e, a = testFilterOn(t, exp, input, "blah")
+	assert.Equal(t, e, a)
 
 	// recursive without value
 	exp = `{
@@ -78,26 +65,23 @@ func TestPath_FilterOn(t *testing.T) {
 			}
 		}
 	}`
-	d1 = testDecoder(t, exp)
-	d2 = testDecoder(t, input)
-	err = d2.FilterOn("store.**.color")
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Log(d1, d2)
-	assert.Equal(t, d1.v, d2.v)
+	e, a = testFilterOn(t, exp, input, "store.**.color")
+	assert.Equal(t, e, a)
 
 	// top level key
-	d1 = testDecoder(t, input)
-	d2 = testDecoder(t, input)
-	err = d2.FilterOn("store")
+	e, a = testFilterOn(t, input, input, "store")
+	assert.Equal(t, e, a)
+}
+
+func testFilterOn(t *testing.T, exp, input string, filter string) (e, a interface{}) {
+	d1 := testDecoder(t, exp)
+	d2 := testDecoder(t, input)
+	err := d2.FilterOn(filter)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	t.Log(d1, d2)
-	assert.Equal(t, d1.v, d2.v)
+	return d1.v, d2.v
 }
