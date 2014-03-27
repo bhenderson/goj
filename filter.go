cbba977ce2079e79e7706609fd5601c67469fe42
bhenderson <bhenderson@yp.com>
2014-03-27 07:31:43 -0700

    Trying to simplify the filter
    handles basic cases.

--- filter.go
+++ filter.go
@@ -46,6 +46,7 @@ func filterPath(v interface{}, arr []pathSel, p *Path) {
 	}
 }
 
+// TODO don't need pointer here I think.
 func wrap(msg string, arr *[]pathSel, v pathSel, cb func()) {
 	pushState(arr, v)
 	cb()
@@ -61,83 +62,9 @@ func popState(arr *[]pathSel) {
 	*arr = (*arr)[:len(*arr)-1]
 }
 
-func filterCompare(arr, sels []pathSel) bool {
-	if len(arr) > len(sels) {
-		return false
-	}
-
-	for i, val := range arr {
-		if !sels[i].Equal(val) {
-			return false
-		}
-	}
-
-	return true
-}
-
 func filterVal(arr []pathSel, p *Path) {
-	var i, j int
-	var x, y pathSel
-
-	for ; i < len(p.sel) && j < len(arr); i, j = i+1, j+1 {
-		x = p.sel[i]
-		y = arr[j]
-
-		switch x.(type) {
-		case *pathRec:
-			i++
-			if i >= len(p.sel) {
-				return
-			}
-			if !filterCompare(arr[j:], p.sel[i:]) {
-				i = i - 2 // retry
-			}
-		default:
-			if !x.Equal(y) {
-				return
-			}
-		}
-	}
-
-	// was last compare true?
-	if !x.Equal(y) {
-		return
-	}
-
-	// eval parent
-	if i < len(p.sel) {
-		x = p.sel[i]
-		if _, ok := x.(*pathParent); ok {
-			i, j = i+1, j-1
-			if _, ok = (arr[j]).(*pathVal); ok {
-				j--
-			}
-		}
-	}
-
-	arr = arr[:j]
-	v := findPath(&arr, p.v)
-
-	if i >= len(p.sel) {
-		if len(arr) > 0 {
-			last := arr[len(arr)-1]
-			if _, ok := last.(*pathVal); !ok {
-				arr = append(arr, &pathVal{v})
-			}
-		}
-		if p.p == "" {
-			p.res = arr
-
-			return
-		}
-	} else {
-		p2 := &Path{sel: p.sel[i:], v: v}
-		var arr2 []pathSel
-		filterPath(v, arr2, p2)
-		arr = append(arr, p2.res...)
-	}
-
-	p.r = buildPath(arr, p.r)
+	matchedPath := pathMatch(arr, p.sel, p)
+	p.r = buildPath(matchedPath, p.r)
 }
 
 func findPath(arrPtr *[]pathSel, v interface{}) interface{} {
@@ -154,4 +81,19 @@ func findPath(arrPtr *[]pathSel, v interface{}) interface{} {
 	}
 
 	return v
+}
+
+func pathMatch(arr, sel []pathSel, p *Path) []pathSel {
+	for i, j := 0, 0; i < len(sel) && j < len(arr); i, j = i+1, j+1 {
+		x, y := sel[i], arr[j]
+		switch x.(type) {
+		case *pathRec:
+		case *pathParent:
+		default:
+			if !x.Equal(y) {
+				return []pathSel{}
+			}
+		}
+	}
+	return arr
 }
