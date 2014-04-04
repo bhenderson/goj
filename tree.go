ab8f53bdcf8676dce2e702d1d7a3f45eb4f6f5e4
bhenderson <bhenderson@yp.com>
2014-04-04 15:16:39 -0700

    Add more docs.

--- tree.go
+++ tree.go
@@ -9,6 +9,19 @@ type branch []*Leaf
 
 const trunkStr = "trunk"
 
+// TODO the public API here needs cleanup.
+
+// Leaf is the main data type to turn a json decoded object into a linked list.
+// Tree is used just as a word to describe the object. Branches are a slice of
+// Leaf pointers. A Branch itself is not a linked list, but each leaf links to
+// it's parent. The value of a leaf is the key, index or value. The child of a
+// leaf is the key or index value.
+//
+// 	obj := // json decoded object
+// 	       // '{"a":{"b":"c"}}'
+// 	Leaf(trunk, val: nil, child: obj)
+// 	\_
+// 	  Leaf(parent: trunk, val: "a", child: {"b": "c"})
 type Leaf struct {
 	parent *Leaf
 	child  interface{}
@@ -24,13 +37,23 @@ const (
 	leafVal
 )
 
-func (l *Leaf) Parent() *Leaf         { return l.parent }
+// Parent returns a pointer to this leaf's parent leaf.
+func (l *Leaf) Parent() *Leaf { return l.parent }
+
+// GetBranch returns all the parents recursively of this leaf including this leaf.
+// There is a special leaf type (the trunk) which will not be included in the
+// branch, but it will still be referenced by the first leaf's parent. This is
+// so that other branches can find each other.
 func (l *Leaf) GetBranch() (b branch) { return getBranch(l) }
 
 // Traverse does a depth first search starting from leaf and yields the end
 // node to the call back function.
 func (l *Leaf) Traverse(cb leafFunc) { traverse(l, cb) }
-func (l *Leaf) Child() interface{}   { return l.child }
+
+// Child returns the interface{} that this leaf points to.
+func (l *Leaf) Child() interface{} { return l.child }
+
+// String is a convenience method for pretty printing a leaf.
 func (l *Leaf) String() string {
 	if l.kind == leafTrk {
 		// trunk
@@ -47,6 +70,8 @@ func (l *Leaf) Branches(cb func(b branch)) {
 	})
 }
 
+// PruneBranches takes a Path pointer and returns a pruned copy of this leaf's
+// child.
 func (l *Leaf) PruneBranches(p *Path) interface{} {
 	v := copyZero(l.Child())
 
@@ -61,6 +86,8 @@ func (l *Leaf) PruneBranches(p *Path) interface{} {
 	return v
 }
 
+// NewTree takes an interface and returns a special Leaf pointer, the trunk,
+// which references all other leaves.
 func NewTree(v interface{}) *Leaf {
 	return &Leaf{child: v, kind: leafTrk}
 }
